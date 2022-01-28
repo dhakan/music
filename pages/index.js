400731271ebefc06b9f963af185c4cf71ac6a23e
Dannie Håkansson <dannie.hakansson@cmeducations.se>
2022-01-28 21:23:46 +0100

    Fixed class issue

--- pages/index.js
+++ pages/index.js
@@ -13,16 +13,16 @@ export default function Index() {
 
         <h2>Chrono Trigger - Sad Theme</h2>
         <small>Key of Dmin (relative to F)</small>
-        <section class={styles.chords}>
+        <section className={styles.chords}>
           <p>
-            <small class={styles.chordHeading}>Intro</small>
+            <small className={styles.chordHeading}>Intro</small>
             Dm9 (i) - Gm11 (iv) - B♭add9 (VI) - Fadd9 (iii) - Gm7 half dim -
             (iv) - Gdim7 (iv)
             <br />
             Dm9 (i) - Gm11 (iv) - B♭add9 (VI) - Cadd9 (VII) - Dsus2 (i) - Dadd9
             (i)
           </p>
-          <small class={styles.chordHeading}>Main</small>
+          <small className={styles.chordHeading}>Main</small>
           Gmadd9 (iv) - Cadd9 (VII) - A7 (V) - Dm (i) - Cadd9 (VII) - B♭add9
           (VI) - Gm (iv) - Csus4 (VII) - C (VII){" "}
         </section>
